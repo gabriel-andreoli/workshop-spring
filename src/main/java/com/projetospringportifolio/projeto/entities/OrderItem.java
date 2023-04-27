7aea99d3fc074f73d22612618acb5f88a53b5802
gabriel-andreoli <gabriandreoli@outlook.com>
2023-04-26 21:30:23 -0300

    Order item, n to n att

--- src/main/java/com/projetospringportifolio/projeto/entities/OrderItem.java
+++ src/main/java/com/projetospringportifolio/projeto/entities/OrderItem.java
@@ -1,5 +1,6 @@
 package com.projetospringportifolio.projeto.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.projetospringportifolio.projeto.entities.pk.OrderItemPK;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -15,7 +16,7 @@ public class OrderItem implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @EmbeddedId
-  private OrderItemPK id;
+  private OrderItemPK id = new OrderItemPK();
 
   private Integer quantity;
   private Double price;
@@ -29,6 +30,7 @@ public class OrderItem implements Serializable {
     this.price = price;
   }
 
+  @JsonIgnore
   public Order getOrder(){
     return id.getOrder();
   }
@@ -44,7 +46,6 @@ public class OrderItem implements Serializable {
   public void setProduct(Product product){
     id.setProduct(product);
   }
-
 
   public Integer getQuantity() {
     return quantity;
