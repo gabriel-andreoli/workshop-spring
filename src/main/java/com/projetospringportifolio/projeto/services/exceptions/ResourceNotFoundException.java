cb90faa9f6cd533ff72673c44d9c016425c4ede7
gabriel-andreoli <gabriandreoli@outlook.com>
2023-05-01 13:41:15 -0300

    exception update

--- src/main/java/com/projetospringportifolio/projeto/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/projetospringportifolio/projeto/services/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.projetospringportifolio.projeto.services.exceptions;
 public class ResourceNotFoundException extends RuntimeException {
 
   public ResourceNotFoundException(Object id){
-    super("Resoruce not found. Id " + id);
+    super("Resource not found. Id " + id);
   }
 
 }
